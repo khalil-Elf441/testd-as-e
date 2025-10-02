c656c1e9a92b7780a401f3a2c50c693ceeb1ff05
khalil EL. <57668350+khalil-Elf441@users.noreply.github.com>
2025-10-02 23:32:08 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // <-- HashRouter pour GitHub Pages
 import Home from "./pages/Home";
-import ExamsPage from "./pages/ExamsPage"; // créer ce composant
+import ExamsPage from "./pages/ExamsPage";         // à créer
+import ExamMockPage from "./pages/ExamMockPage";   // à créer
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exams/:tech" element={<ExamsPage />} />
+        <Route path="/exams/:tech/:examCode" element={<ExamMockPage />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
