d315ddc1566268cdf329e370ad0e430c94c990ce
khalil EL. <57668350+khalil-Elf441@users.noreply.github.com>
2025-10-02 23:23:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ExamsPage from "./pages/ExamsPage"; // créer ce composant
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Ici tu ajoutes les routes vers Exams, ExamMocks, etc */}
+        <Route path="/exams/:tech" element={<ExamsPage />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
